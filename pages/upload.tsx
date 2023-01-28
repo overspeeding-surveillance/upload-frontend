7da0abe4d5605d5937635872f8aa832acce69c8a
Aabhas Dhaubanja <aabhas41@gmail.com>
2023-01-28 19:29:34 +0545

    feat: add drag and drop component

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -2,18 +2,57 @@ import React from "react";
 import styles from "../styles/upload.module.scss";
 
 const Upload = () => {
+  const [file, setFile] = React.useState<File>();
+  const [] = React.useState();
+
+  const _handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+  const _handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setFile(event.dataTransfer.files[0]);
+  };
+
+  const _handleBrowse = () => inputRef.current?.click();
+
+  const _handleUpload = () => {
+    console.log(file);
+  };
+
+  const _handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.files?.[0] && setFile(event.target.files[0]);
+  };
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
   return (
     <div className={styles.uploadWrapper}>
       <div className={styles.uploadContainer}>
-        <div className={styles.drag}>
+        <div
+          onDragOver={_handleDragOver}
+          onDrop={_handleDrop}
+          className={styles.drag}
+        >
           <h1>
             Drag & drop&nbsp;<span className={styles.videos}>videos</span>
           </h1>
           <div>
-            or&nbsp;<span className={styles.browse}>browse files </span>&nbsp;on
-            your computer
+            or&nbsp;
+            <span onClick={_handleBrowse} className={styles.browse}>
+              browse files&nbsp;
+            </span>
+            &nbsp;on your computer
           </div>
+          <input
+            onChange={_handleFileChange}
+            ref={inputRef}
+            type="file"
+            hidden
+          />
         </div>
+        <button onClick={_handleUpload} className={styles.button}>
+          Upload
+        </button>
       </div>
     </div>
   );
