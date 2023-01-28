cc8c5d70fafe473146a129f1aa3fdd6a9754820e
Aabhas Dhaubanja <aabhas41@gmail.com>
2023-01-28 19:02:58 +0545

    feat: add upload page UI

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "../styles/upload.module.scss";
 
 const Upload = () => {
-  return <div>upload</div>;
+  return (
+    <div className={styles.uploadWrapper}>
+      <div className={styles.uploadContainer}>
+        <div className={styles.drag}>
+          <h1>
+            Drag & drop&nbsp;<span className={styles.videos}>videos</span>
+          </h1>
+          <div>
+            or&nbsp;<span className={styles.browse}>browse files </span>&nbsp;on
+            your computer
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Upload;
